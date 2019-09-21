c90d8c1f9af0209caa6dd87b839565184b219696
Jahn08 <jahn_08@mail.ru>
2019-09-21 18:42:21 +0300

    Fixing a database context construction.

--- WebArticleLibrary.Model/ArticleLibraryContext.cs
+++ WebArticleLibrary.Model/ArticleLibraryContext.cs
@@ -2,8 +2,6 @@ namespace WebArticleLibrary.Model
 {
 	using System;
 	using Microsoft.EntityFrameworkCore;
-	using System.Linq;
-    using Microsoft.Extensions.Configuration;
 
 	public partial class ArticleLibraryContext : DbContext, IDisposable
 	{
@@ -14,33 +12,15 @@ namespace WebArticleLibrary.Model
             //new MigrateDatabaseToLatestVersion<ArticleLibraryContext, Configuration>());
 		}
 
+		public ArticleLibraryContext(DbContextOptions<ArticleLibraryContext> options): base(options)
+		{
+		}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
                 optionsBuilder.UseNpgsql("User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=weblibrary;");
         }
-
-		public delegate void OnNotificationAdded(UserNotification[] etities);
-		
-        public event OnNotificationAdded OnNotificationAddedEvent;
-
-		public override int SaveChanges()
-		{
-			var outcome = base.SaveChanges();
-
-			var _event = OnNotificationAddedEvent;
-
-			if (_event != null)
-			{
-				var etities = this.ChangeTracker.Entries<UserNotification>()
-                    .Select(e => e.Entity).ToArray();
-
-				if (etities.Any())
-					_event(etities);
-			}
-
-			return outcome;
-		}
 
 		public void Close()
 		{
